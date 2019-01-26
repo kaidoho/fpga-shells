0d0c1b4b0b66ee3ca659a9309302e439ce60456b
kaidoho <kho237115@gmail.com>
2019-01-26 12:36:01 +0100

    FIXED: board used wrong XILINX part

--- xilinx/arty_s7_50/tcl/board.tcl
+++ xilinx/arty_s7_50/tcl/board.tcl
@@ -1,5 +1,5 @@
 # See LICENSE for license details.
 set name {arty}
-set part_fpga {xc7s50csga324-1L}
-set part_board {digilentinc.com:arty-s7-50:part0:1.1}
+set part_fpga {xc7s50csga324-1}
+set part_board {digilentinc.com:arty-s7-50:part0:1.0}
 set bootrom_inst {rom}
