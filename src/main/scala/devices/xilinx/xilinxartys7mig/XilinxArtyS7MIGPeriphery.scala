c51dc5a3f9216e7ff769604d8cb8dbeda8cbf0ba
kaidoho <kho237115@gmail.com>
2019-02-08 11:43:35 +0100

    ADDED Xilinx MIG for ARTY S7

---
+++ src/main/scala/devices/xilinx/xilinxartys7mig/XilinxArtyS7MIGPeriphery.scala
@@ -0,0 +1,36 @@
+// See LICENSE for license details.
+package sifive.fpgashells.devices.xilinx.xilinxartys7mig
+
+import Chisel._
+import freechips.rocketchip.config._
+import freechips.rocketchip.subsystem.BaseSubsystem
+import freechips.rocketchip.diplomacy.{LazyModule, LazyModuleImp, AddressRange}
+
+case object MemoryXilinxDDRKey extends Field[XilinxArtyS7MIGParams]
+
+trait HasMemoryXilinxArtyS7MIG { this: BaseSubsystem =>
+  val module: HasMemoryXilinxArtyS7MIGModuleImp
+
+  val xilinxartys7mig = LazyModule(new XilinxArtyS7MIG(p(MemoryXilinxDDRKey)))
+
+  xilinxartys7mig.node := mbus.toDRAMController(Some("xilinxartys7mig"))()
+}
+
+trait HasMemoryXilinxArtyS7MIGBundle {
+  val xilinxartys7mig: XilinxArtyS7MIGIO
+  def connectXilinxArtyS7MIGToPads(pads: XilinxArtyS7MIGPads) {
+    pads <> xilinxartys7mig
+  }
+}
+
+trait HasMemoryXilinxArtyS7MIGModuleImp extends LazyModuleImp
+    with HasMemoryXilinxArtyS7MIGBundle {
+  val outer: HasMemoryXilinxArtyS7MIG
+  val ranges = AddressRange.fromSets(p(MemoryXilinxDDRKey).address)
+  require (ranges.size == 1, "DDR range must be contiguous")
+  val depth = ranges.head.size
+  val xilinxartys7mig = IO(new XilinxArtyS7MIGIO(depth))
+
+  xilinxartys7mig <> outer.xilinxartys7mig.module.io.port
+}
+
